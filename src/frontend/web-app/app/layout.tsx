1c22ee506ff0d21decd3d0acdbfbf1cd581b75c8
jeeva <jeevaprakashdr@gmail.com>
2026-01-18 18:01:36 +0100

    init  web app

---
+++ src/frontend/web-app/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Image Gallery",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <main className="max-w-6xl mx-auto">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
