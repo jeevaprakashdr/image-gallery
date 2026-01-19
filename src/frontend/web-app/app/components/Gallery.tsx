5f336d5e78cf69afaebb67d62fdfd13bc61340fd
jeeva <jeevaprakashdr@gmail.com>
2026-01-19 12:16:59 +0100

    client component with websoocket connection

--- src/frontend/web-app/app/components/Gallery.tsx
+++ src/frontend/web-app/app/components/Gallery.tsx
@@ -1,23 +1,9 @@
-
-import fetchImages from "@/lib/fetchImages"
-import { Image } from "@/models/Image"
-import ImgContainer from "./ImgContainer";
+import GalleryClient from "./GalleryClient";
 
 type Props = {
     topic?: string | undefined
 }
 
 export default async function Gallery({ topic } : Props) {
-    const images: Image[] | undefined= await fetchImages(topic);
-    
-    if(!images)
-        return <h2 className="m4 text-2xl font-bold">No Images found</h2>
-    
-    return (
-        <section className="px-2 my-3 grid grid-cols-3 gap-5">
-            {images.map((x, i) => (
-                <ImgContainer key={i} photo={x}/>
-            ))}
-        </section>
-    )
+    return <GalleryClient topic={topic} />;
 }
---
+++ src/frontend/web-app/app/components/GalleryClient.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import fetchImages from "@/lib/fetchImages";
+import { Image } from "@/models/Image";
+import ImgContainer from "./ImgContainer";
+
+type Props = {
+    topic?: string | undefined
+}
+
+export default function GalleryClient({ topic }: Props) {
+    const [images, setImages] = useState<Image[] | undefined>(undefined);
+
+    useEffect(() => {
+        fetchImages(topic).then(setImages);
+
+        const ws = new WebSocket("ws://localhost:8081/ws");
+
+        ws.onmessage = (event) => {
+            const newImage = new Image();
+            newImage.id = event.data
+            setImages(prev =>
+                prev ? [...prev, newImage] : [newImage]
+            );
+        };
+
+        return () => ws.close();
+    }, [topic]);
+
+    if (!images)
+        return <h2 className="m4 text-2xl font-bold">No Images found</h2>;
+
+    return (
+        <section className="px-2 my-3 grid grid-cols-3 gap-5">
+            {images.map((x, i) => (
+                <ImgContainer key={i} photo={x} />
+            ))}
+        </section>
+    );
+}
