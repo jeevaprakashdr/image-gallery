299fabe2faaab383332f7706a8a3eff2e97217cb
jeeva <jeevaprakashdr@gmail.com>
2026-01-19 12:46:54 +0100

    configured environment variables

--- src/websocket/go.mod
+++ src/websocket/go.mod
@@ -2,4 +2,7 @@ module github.com/jeevaprakashdr/image-gallery-websocket
 
 go 1.25.6
 
-require github.com/gorilla/websocket v1.5.3
+require (
+	github.com/gorilla/websocket v1.5.3
+	github.com/joho/godotenv v1.5.1
+)
--- src/websocket/main.go
+++ src/websocket/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"sync"
 
 	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
 )
 
 var upgrader = websocket.Upgrader{}
@@ -14,12 +17,19 @@ var broadcast = make(chan []byte)
 var mu sync.Mutex
 
 func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("WebsocketServer: Error loading .env file: %v", err)
+	}
+
 	http.HandleFunc("/ws", handleConnections)
 	go handleMessages()
 
-	fmt.Println("WebSocket Server Started on :8081")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
-		fmt.Println("WebSocket Server Failed to Start.")
+	wsAddress := os.Getenv("WS_ADDRESS")
+
+	fmt.Printf("WebsocketServer: Started on :%s\n", wsAddress)
+	if err := http.ListenAndServe(wsAddress, nil); err != nil {
+		fmt.Println("WebsocketServer: Failed to Start.")
 	}
 }
 
